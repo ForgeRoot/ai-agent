b83be57e87321aeedba5a7fd2be0b5c2e5ba1e8a
ForgeRoot <dylananne.v.eck@gmail.com>
2025-10-03 13:20:33 +0200

    add an verbose option

--- main.py
+++ main.py
@@ -19,23 +19,29 @@ def main():
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
 
-    user_prompt = " ".join(args)
+    if args[-1] == "--verbose":
+        user_prompt = " ".join(args[:-1])
+    else:
+        user_prompt = " ".join(args)
+
     messages = [
         types.Content(role="user", parts=[types.Part(text=user_prompt)]),
     ]
+    response = generate_response(client, messages)
 
-    generate_content(client, messages)
+    if args[-1] == "--verbose":
+        print(f"User prompt: {user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
-def generate_content(client, messages):
-    response = client.models.generate_content(
+    print("Response:")
+    print(response.text)
+
+def generate_response(client, messages):
+    return client.models.generate_content(
         model="gemini-2.0-flash-001", 
         contents=messages
     )
-    
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-    print("Response:")
-    print(response.text)
 
 if __name__ == "__main__":
     main()
