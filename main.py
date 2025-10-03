9af0f4545b53d9fbbc74ace8f04c798a52bed3ff
ForgeRoot <dylananne.v.eck@gmail.com>
2025-10-03 09:25:00 +0200

    implemented command line input

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
 
@@ -6,12 +7,16 @@ from google import genai
 def main():
     load_dotenv()
 
+    if len(sys.argv) == 1:
+        print("You didn't provided a prompt")
+        sys.exit(1)
+
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
 
     response = client.models.generate_content(
         model="gemini-2.0-flash-001", 
-        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+        contents=sys.argv[1]
     )
     
     print(response.text)
