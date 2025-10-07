7ca5cf7989a609a2fe9f788950790bda7cbe5cd4
ForgeRoot <dylananne.v.eck@gmail.com>
2025-10-07 13:15:02 +0200

    refactored get file content

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -1,5 +1,24 @@
 import os
 from config import MAX_CHARS
+
+def get_file_content(working_directory, file_path):
+    abs_working_dir = os.path.abspath(working_directory)
+    abs_file_path = os.path.abspath(os.path.join(working_directory,file_path))
+    if not abs_file_path.startswith(abs_working_dir):
+        return f'Error: Cannot read \"{file_path}\" as it is outside the permitted working directory'
+    if not os.path.isfile(abs_file_path):
+        return f'Error: File not found or is not a regular file: \"{file_path}\"'
+    try:
+        with open(target_file, "r") as f:
+            content = f.read(MAX_CHARS)
+            if os.path.getsize(target_file) > MAX_CHARS:
+                content += (
+                    f"[...File \"{file_path}\" truncated at {MAX_CHARS} characters]"
+                )
+        return content
+    except Exception as e:
+        return f'Error: {e}'
+
 from google.genai import types
 
 schema_get_file_content = types.FunctionDeclaration(
@@ -15,21 +34,3 @@ schema_get_file_content = types.FunctionDeclaration(
         },
     ),
 )
-
-def get_file_content(working_directory, file_path):
-    target_file = os.path.abspath(os.path.join(working_directory,file_path))
-    working_dir_path = os.path.abspath(working_directory)
-    if not target_file.startswith(working_dir_path):
-        return f'Error: Cannot read \"{file_path}\" as it is outside the permitted working directory'
-    if not os.path.isfile(target_file):
-        return f'Error: File not found or is not a regular file: \"{file_path}\"'
-    try:
-        with open(target_file, "r") as f:
-            content = f.read(MAX_CHARS)
-            if os.path.getsize(target_file) > MAX_CHARS:
-                content += (
-                    f"[...File \"{file_path}\" truncated at {MAX_CHARS} characters]"
-                )
-        return content
-    except Exception as e:
-        return f'Error: {e}'
