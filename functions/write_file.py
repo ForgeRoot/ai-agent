da99c9509c56ad8802a09df620879f7963fd6d6c
ForgeRoot <dylananne.v.eck@gmail.com>
2025-10-07 13:13:00 +0200

    refactored write file

--- functions/write_file.py
+++ functions/write_file.py
@@ -3,12 +3,19 @@ from google.genai import types
 
 
 def write_file(working_directory, file_path, content):
-    target_file = os.path.abspath(os.path.join(working_directory,file_path))
-    working_dir_path = os.path.abspath(working_directory)
-    if not target_file.startswith(working_dir_path):
+    abs_working_dir = os.path.abspath(working_directory)
+    abs_file_path = os.path.abspath(os.path.join(working_directory,file_path))
+    if not abs_file_path.startswith(abs_working_dir):
         return f'Error: Cannot read \"{file_path}\" as it is outside the permitted working directory'
+    if not os.path.exists(abs_file_path):
+        try:
+            os.makedirs(os.path.dirname(abs_file_path), exist_ok=True)
+        except Exception as e:
+            return f"Error: creating directory: {e}"
+    if os.path.exists(abs_file_path) and os.path.isdir(abs_file_path):
+        return f'Error: "{file_path}" is a directory, not a file'
     try:
-        with open(target_file, "w") as f:
+        with open(abs_file_path, "w") as f:
             f.write(content)
         return(f'Successfully wrote to "{file_path}" ({len(content)} characters written)')
     except Exception as e:
