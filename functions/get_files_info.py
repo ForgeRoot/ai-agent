fde1a789d8fe8e47ff6be7176c0e3e46810dc12b
ForgeRoot <dylananne.v.eck@gmail.com>
2025-10-07 13:22:30 +0200

    refactored get files

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,6 +1,29 @@
 import os
 from google.genai import types
 
+
+def get_files_info(working_directory, directory="."):
+    abs_working_dir = os.path.abspath(working_directory)
+    target_dir = os.path.abspath(os.path.join(working_directory, directory))
+    if not target_dir.startswith(abs_working_dir):
+        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+    if not os.path.isdir(target_dir):
+        return f'Error: "{directory}" is not a directory'
+    try:
+        files_info = []
+        for filename in os.listdir(target_dir):
+            filepath = os.path.join(target_dir, filename):
+            file_size = 0
+            is_dir = os.path.isdir(filepath)
+            file_size = os.path.getsize(filepath)
+            files_info.append(
+                f"- {filename}: file_size={file_size} bytes, is_dir={is_dir}"
+            )
+        return "\n".join(files_info)
+    except Exception as e:
+        return f'Error listing files: {e}'
+
+
 schema_get_files_info = types.FunctionDeclaration(
     name="get_files_info",
     description="Lists multiple files and directories with their sizes",
@@ -14,27 +37,3 @@ schema_get_files_info = types.FunctionDeclaration(
         },
     ),
 )
-
-def get_files_info(working_directory, directory="."):
-    target_dir = os.path.abspath(os.path.join(working_directory, directory))
-    working_dir_path = os.path.abspath(working_directory)
-
-    if not target_dir.startswith(working_dir_path):
-        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
-    if not os.path.isdir(target_dir):
-        return f'Error: "{directory}" is not a directory'
-    try:
-        entries = sorted(os.listdir(target_dir))
-        mapped = "\n".join(generate_line(name, target_dir) for name in entries)
-        return mapped
-
-    except Exception as e:
-        return f'Error: {e}'
-
-def generate_line(name, full_path):
-    full_path_of_file = os.path.join(full_path, name)
-    size = os.path.getsize(full_path_of_file)
-    is_dir = os.path.isdir(full_path_of_file)
-
-    return f"- {name}: file_size={size} bytes, is_dir={is_dir}"
-    
